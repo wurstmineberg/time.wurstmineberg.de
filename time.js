209851b955e99f455b33b0ea3d68e29fcafc5d17
Fenhl <fenhl@fenhl.net>
2013-11-03 21:12:18 +0000

    Add syntax

--- time.js
+++ time.js
@@ -1,18 +1,18 @@
 $.ajax('http://api.wurstmineberg.de/server/level.json', {
     dataType: 'json',
     success: function(data) {
-        var timeticks = -1
+        var timeticks = -1;
         if ('Data' in data) {
             if ('DayTime' in data['Data']) {
-                timeticks = data['Data']['DayTime']
+                timeticks = data['Data']['DayTime'];
             }
         }
 
         if (timeticks == -1) {
-            $('#time-caption').text("???")
-            $('#time-text').text("I have no idea. Seriously. Something is broken")
+            $('#time-caption').text("???");
+            $('#time-text').text("I have no idea. Seriously. Something is broken");
         } else {
-            $('#time-text').text($(timeticks).val())
+            $('#time-text').text(timeticks);
         } 
     }
 });
