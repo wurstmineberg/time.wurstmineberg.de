2eaad46c857b1dc13f55a4fe1590da38e96bf7f6
Fenhl <fenhl@fenhl.net>
2013-11-04 10:20:04 +0000

    Add ticks since sunrise back in
    Also tabs to spaces because, srsly?

--- assets/time.js
+++ assets/time.js
@@ -2,47 +2,48 @@ getData();
 setTimeout('getData', 30000);
 
 function rotate(degrees) {
-	$('.dial').css({
-		'-webkit-transform' : 'rotate('+degrees+'deg)',
-		'-moz-transform' : 'rotate('+degrees+'deg)',
-		'-ms-transform' : 'rotate('+degrees+'deg)',
-		'-o-transform' : 'rotate('+degrees+'deg)',
-		'transform' : 'rotate('+degrees+'deg)',
-	});
+    $('.dial').css({
+        '-webkit-transform' : 'rotate('+degrees+'deg)',
+        '-moz-transform' : 'rotate('+degrees+'deg)',
+        '-ms-transform' : 'rotate('+degrees+'deg)',
+        '-o-transform' : 'rotate('+degrees+'deg)',
+        'transform' : 'rotate('+degrees+'deg)',
+    });
 }
 
 function getData() {
-	$.ajax('http://api.wurstmineberg.de/server/level.json', {
-		dataType: 'json',
-		error: function(request, status, error) {
-			$('#time-caption').html("???");
-			$('#time-text').html("Could not load level.json");
-		},
-		success: function(data) {
-			var timeticks = -1;
-			if ('Data' in data) {
-				if ('DayTime' in data['Data']) {
-					timeticks = data['Data']['DayTime'];
-				}
-			}
+    $.ajax('http://api.wurstmineberg.de/server/level.json', {
+        dataType: 'json',
+        error: function(request, status, error) {
+            $('#time-caption').html("???");
+            $('#time-text').html("Could not load level.json");
+        },
+        success: function(data) {
+            var timeticks = -1;
+            if ('Data' in data) {
+                if ('DayTime' in data['Data']) {
+                    timeticks = data['Data']['DayTime'];
+                }
+            }
 
-			if (timeticks == -1) {
-				$('#time-caption').html("???");
-				$('#time-text').html("I have no idea. Seriously. Something is broken");
-			} else {
-				var ticksSinceSunrise = timeticks % 24000;
-				var secondsSinceSunrise = Math.floor(ticksSinceSunrise / 20);
-				var hoursSinceSunrise = Math.floor(secondsSinceSunrise / 50);
-				var secondsSinceHour = secondsSinceSunrise % 50;
-				hoursSinceSunrise = (hoursSinceSunrise < 10 ? '0' : '') + hoursSinceSunrise;
-				secondsSinceHour = (secondsSinceHour < 10 ? '0' : '') + secondsSinceHour;
+            if (timeticks == -1) {
+                $('#time-caption').html("???");
+                $('#time-text').html("I have no idea. Seriously. Something is broken");
+            } else {
+                var ticksSinceSunrise = timeticks % 24000;
+                var secondsSinceSunrise = Math.floor(ticksSinceSunrise / 20);
+                var hoursSinceSunrise = Math.floor(secondsSinceSunrise / 50);
+                var secondsSinceHour = secondsSinceSunrise % 50;
+                hoursSinceSunrise = (hoursSinceSunrise < 10 ? '0' : '') + hoursSinceSunrise;
+                secondsSinceHour = (secondsSinceHour < 10 ? '0' : '') + secondsSinceHour;
 
-				dg = (parseInt((timeticks % 24000) / 1000) * 360 / 24) - 180;
-				rotate(dg);
-				console.log(dg);
-				$('#time-caption').append(hoursSinceSunrise + ':' + secondsSinceHour);
-				$('#ticks-text').html(timeticks + ' ticks since the beginning of time');
-			} 
-		}
-	});
+                dg = (parseInt((timeticks % 24000) / 1000) * 360 / 24) - 180;
+                rotate(dg);
+                console.log(dg);
+                $('#time-caption').append(hoursSinceSunrise + ':' + secondsSinceHour);
+                $('#time-text').html(ticksSinceSunrise + ' ticks');
+                $('#ticks-text').html(timeticks + ' ticks since the beginning of time');
+            } 
+        }
+    });
 }
